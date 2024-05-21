323028a9594c76843f0e58f2b5502a0502284e91
Mohamed El-Serafy <elserafy02@gmail.com>
2024-05-21 14:44:09 +0300

    day 4

---
+++ day 4/1.1/script.js
@@ -0,0 +1,51 @@
+
+
+function checkFormat(userInput) {
+    console.log(userInput.length)
+    if (userInput.length != 10) {
+        console.log(userInput.length);
+        return false
+    }
+    else if (userInput[2] == '-' && userInput[5] == '-') {
+        let newArray = userInput.split("-")
+        for (let i = 0; i < newArray.length; i++) {
+            if (isNaN(newArray[i])) {
+                return false;
+            }
+        }
+
+        let dateObject = { day: Number(newArray[0]), month: Number((newArray[1] - 1)), year: Number(newArray[2]) }
+
+        let newDate = new Date(dateObject.year, dateObject.month, dateObject.day);
+        if (newDate.getFullYear() == dateObject.year &&
+            newDate.getMonth() == dateObject.month &&
+            newDate.getDate() == dateObject.day) {
+            return newDate;
+        } else {
+            console.log(newDate)
+            return false;
+        }
+
+    }
+
+}
+function test() {
+    let birth_date
+    while (!birth_date|| birth_date != 0) {
+        birth_date   = prompt("enter your birth date in the DD-MM-YYYY format");
+
+        let userCorrect = checkFormat(birth_date);
+
+        if (!userCorrect) {
+            alert("your format is wrong")
+        } else {
+            console.log(userCorrect)
+            alert(userCorrect.toString());
+
+        }
+    }
+}
+
+test()
+
+
---
+++ day 4/1.2/script.js
@@ -0,0 +1,38 @@
+// function checkFormat(dateString) {
+//     console.log(userInput.length)
+  
+
+//         let newDate = new Date(dateObject.year, dateObject.month, dateObject.day);
+//         if (newDate.getFullYear() == dateObject.year &&
+//             newDate.getMonth() == dateObject.month &&
+//             newDate.getDate() == dateObject.day) {
+//             return newDate;
+//         } else {
+//             console.log(newDate)
+//             return false;
+//         }
+
+    
+
+// }
+
+
+
+function takeDateAndReturnDay(datString){
+
+    let newDate = new Date(datString)
+    console.log(newDate)
+    console.log(newDate.getDate())
+    console.log(newDate.getMonth())
+    console.log(newDate.getUTCDate())
+
+    
+    let dayName =  newDate.toLocaleString('en-us', {weekday:'long'})
+    console.log(dayName)
+    return  dayName
+
+}
+
+takeDateAndReturnDay("1999-10-15")
+
+takeDateAndReturnDay("1999-09-31") //wrong date
